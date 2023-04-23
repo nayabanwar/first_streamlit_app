ead875dd60eef8d1fc1d1d986bd0e17162f61381
Nayab Anwar <72248277+nayabanwar@users.noreply.github.com>
2023-04-23 20:36:25 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -10,6 +10,7 @@ import pandas
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+streamlit.header("Fruityvice Fruit Advice!")
                       
           
