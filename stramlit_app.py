a183c2d035d750f084b0934adb75c727fb3731fc
Nayab Anwar <72248277+nayabanwar@users.noreply.github.com>
2023-04-24 15:21:48 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -35,7 +35,7 @@ my_data_row=my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 my_data_row=my_cur.fetchone()
-stramlit.header("The fruit load list contains:")
+streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 
